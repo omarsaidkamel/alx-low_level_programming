506d6bb000bd0636460a047800624c0998b11383
omarsaidkamel <osk09876@gmail.com>
2023-08-14 08:42:08 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a'; int a;
+	for (a=0; a<26; a++)
+		putcahr(c+a);
+	return (0);
+}
