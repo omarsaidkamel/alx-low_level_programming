0e071439abf8f5ac36673356b5e68ea78c32ac0f
omarsaidkamel <osk09876@gmail.com>
2023-08-15 09:57:35 -0700

    done

---
+++ 0x02-functions_nested_loops/0-main.c
@@ -0,0 +1,13 @@
+#include "omar.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print();
+	return (0);
+}
--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include<main.h>
+#include "omar.h"
 
 /**
- * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
- * Return: 0.
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
  */
 
-int main(void) {
+int print() {
 	putchar('_');
 	putchar('p');
 	putchar('u');
