8846b54750b8db7c2d3c64c70dd34731bf08f76c
omarsaidkamel <osk09876@gmail.com>
2023-08-15 08:37:13 -0700

    done

---
+++ 0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
