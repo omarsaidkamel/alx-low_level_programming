4e1ad748eff2322daf9f31a34ef865f4a3a73653
omarsaidkamel <osk09876@gmail.com>
2023-08-15 10:43:37 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "omar.h"
+#include "main.h"
 
 /**
  * print_alphabet - Check description
@@ -7,14 +7,14 @@
  */
 
 int print() {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return 0;
 }
