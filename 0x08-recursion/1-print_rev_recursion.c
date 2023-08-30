1923fb6a348c951432e5c29e557cd4a88af2d2ba
omarsaidkamel <osk09876@gmail.com>
2023-08-30 23:02:26 +0300

    done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - a function that prints a string, 
+ * _print_rev_recursion - a function that prints a string,
  * followed by a new line.
  * @s: An input string to printing
  * Return: Nothing
@@ -13,4 +13,4 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-} 
+}
