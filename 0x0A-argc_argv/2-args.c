e29d125bfb97af25d1775e5accdd2c76900d70c3
omarsaidkamel <osk09876@gmail.com>
2023-09-05 09:09:47 -0700

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that prints the number of arguements you passed into it
+ * @argc: An argument counter
+ * @argv: An argument values
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	for(int i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
