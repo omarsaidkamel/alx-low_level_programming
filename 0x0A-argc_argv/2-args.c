6030cb0d0dc7614d30709976fb0e75dbb4ab970f
omarsaidkamel <osk09876@gmail.com>
2023-09-05 09:12:41 -0700

    done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - A program that prints the number of arguements you passed into it
- * @argc: An argument counter
- * @argv: An argument values
+ * main - A program that prints all arguements it receives
+ * @argc: The arguement counter
+ * @argv: The arguement values
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	for(int i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
 	return (0);
 }
