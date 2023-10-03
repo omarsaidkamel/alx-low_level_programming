9f875bda6643675a94e76ab7f1bf25d5c8ca8c8a
omarsaidkamel <osk09876@gmail.com>
2023-10-03 11:06:56 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,9 @@ int create_file(const char *filename, char *text_content)
 
 	x = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	
+	if(x == -1)
+		return (-1);
+
 	while (text_content && *(text_content + l))
 		l++;
 	
